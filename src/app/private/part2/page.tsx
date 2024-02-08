727248da96eaa70dc7040744881c3e0e2f2f08ba
Melvin2306 <melvin.rinkleff@gmx.de>
2024-02-08 18:27:55 +0100

    added challenge in robots and /private/part2

---
+++ src/app/private/part2/page.tsx
@@ -0,0 +1,12 @@
+import Head from 'next/head';
+
+export default function Index() {
+  return (
+    <>
+      <div style={{ padding: 30 }}>
+        <h1>There is nothing to see here 👀</h1>
+        <h2 className="opacity-0">OBot?&#125;</h2>
+      </div>
+    </>
+  );
+}
